d22e65f55374a903c711c22e1ee72d54f6d98c38
BL4Z32216 <167974699+BL4Z32216@users.noreply.github.com>
2024-04-30 12:45:48 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  Profile,
+  Navbar,
+  ThemeProvider,
+  Footer,
+  Home,
+  Watch,
+  Search,
+  Page404,
+  About,
+  PolicyTerms,
+  ShortcutsPopup,
+  ScrollToTop,
+  usePreserveScrollOnReload,
+  Callback,
+  ApolloClientProvider,
+  Settings,
+} from './index';
+import { register } from 'swiper/element/bundle';
+import { Analytics } from '@vercel/analytics/react';
+import { AuthProvider } from './client/useAuth';
+register();
+
+function App() {
+  usePreserveScrollOnReload();
+
+  return (
+    <ApolloClientProvider>
+      <Router>
+        <AuthProvider>
+          <ThemeProvider>
+            <Navbar />
+            <ShortcutsPopup />
+            <ScrollToTop />
+            <div style={{ minHeight: '35rem' }}>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/home' element={<Home />} />
+                <Route path='/search' element={<Search />} />
+                <Route path='/watch/:animeId' element={<Watch />} />
+                <Route
+                  path='/watch/:animeId/:animeTitle/:episodeNumber'
+                  element={<Watch />}
+                />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/profile/settings' element={<Settings />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/pptos' element={<PolicyTerms />} />
+                <Route path='/callback' element={<Callback />} />
+                <Route path='*' element={<Page404 />} />
+              </Routes>
+            </div>
+            <Footer />
+          </ThemeProvider>
+        </AuthProvider>
+      </Router>
+      <Analytics />
+    </ApolloClientProvider>
+  );
+}
+
+export default App;
